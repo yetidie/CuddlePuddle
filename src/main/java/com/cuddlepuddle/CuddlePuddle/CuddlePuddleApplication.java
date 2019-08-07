91bc8d081001be0eb9737225a332a36cb5956b8d
Jezebel <zombieturpentine@gmail.com>
2019-08-07 14:52:07 -0500

    this is a commit

---
+++ src/main/java/com/cuddlepuddle/CuddlePuddle/CuddlePuddleApplication.java
@@ -0,0 +1,13 @@
+package com.cuddlepuddle.CuddlePuddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuddlePuddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuddlePuddleApplication.class, args);
+	}
+
+}
