6a11ac9089d22e65cc407a2d680478c49db42225
Jezebel <zombieturpentine@gmail.com>
2019-08-07 19:14:25 -0500

    commiting to cuddle puddle

--- src/main/java/com/cuddlepuddle/CuddlePuddle/Controller/CuddlePuddleController.java
+++ src/main/java/com/cuddlepuddle/CuddlePuddle/Controller/CuddlePuddleController.java
@@ -1,4 +1,21 @@
 package com.cuddlepuddle.CuddlePuddle.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+@Controller
 public class CuddlePuddleController {
+    @RequestMapping(value = "");
+    public static void index(Model model){
+       @HashMap
+        return 'index.html';
+
+    }
+    @RequestMapping(value = "BathProducts")
+    public static void bathProducts(){
+        return 'BathProducts.html';
+
+    }
 }
